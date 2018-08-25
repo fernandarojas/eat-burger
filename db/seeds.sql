004ce1dfcbcfb9c69a1a6efad72c812f51eec7a7
fernandarojas <fernandarojasc@gmail.com>
2018-08-24 22:31:20 -0400

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES('Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES('Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Chedar', true);
